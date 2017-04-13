4f0f961d11f0e8fa4a7d66c29e5c97b3d9a85804
潘松鹏 <bjpansongpeng@163.com>
2017-04-13 16:51:12 +0800

    参数实例功能 ，更改员工管理Mobile 为long

--- work-web/src/main/java/cn/bjtc/controller/IdxrateViewController.java
+++ work-web/src/main/java/cn/bjtc/controller/IdxrateViewController.java
@@ -8,22 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("supp")
-public class SupplierViewController {
+@RequestMapping("idxrate")
+public class IdxrateViewController {
 
 	@RequestMapping(value="show.html",method=RequestMethod.GET)
-	public String showSupplierView(){
-		return "supplier/list";
+	public String showIdxrateView(){
+		return "idxrate/list";
 	}
 	
 	@RequestMapping(value="add.html",method=RequestMethod.GET)
-	public String showSupplierAddView(){
-		return "supplier/add";
+	public String showIdxrateAddView(){
+		return "idxrate/add";
 	}
 	
-	@RequestMapping(value="{suppid}.html",method=RequestMethod.GET)
-	public String showSupplierEditView(@PathVariable Integer suppid,Model model){
-		model.addAttribute("suppid", suppid);
-		return "supplier/edit";
+	@RequestMapping(value="{id}.html",method=RequestMethod.GET)
+	public String showIdxrateEditView(@PathVariable Integer id,Model model){
+		model.addAttribute("id", id);
+		return "idxrate/edit";
 	}
 }

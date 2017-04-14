620453a5dc2e87ccd154f7b04082c5c02ee350f8
fischerdong <defix34@163.com>
2017-04-14 16:22:49 +0800

    动态化左侧菜单

--- work-webapi/src/main/java/cn/bjtc/load/StartupLoader.java
+++ work-webapi/src/main/java/cn/bjtc/load/StartupLoader.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import cn.bjtc.api.ApiManager;
 import cn.bjtc.model.Api;
+import cn.bjtc.model.Menu;
 import cn.bjtc.model.SysParam;
 import cn.bjtc.service.IApiService;
+import cn.bjtc.service.IMenuService;
 import cn.bjtc.service.ISysParamService;
 import cn.bjtc.tools.ApplicationDataManager;
 import cn.bjtc.view.ApiView;
+import cn.bjtc.view.MenuView;
 import cn.bjtc.view.SysParamView;
 
 public class StartupLoader {
@@ -18,6 +21,7 @@ public class StartupLoader {
 	public void init(){
 		initApiMap();
 		initSysParam();
+		initSysMenus();
 	}
 	
 	public void initApiMap(){
@@ -39,8 +43,27 @@ public class StartupLoader {
 		}
 	}
 	
+	public void initSysMenus(){
+		MenuView view = new MenuView();
+		view.setParentid(0);
+		view.setPageSize(50);
+		view.setMenust(1);
+		List<Menu> pmenus = menuService.findAllMenus(view);
+		for(Menu parent : pmenus){
+			MenuView view2 = new MenuView();
+			view2.setParentid(parent.getMenuid());
+			view2.setPageSize(50);
+			view2.setMenust(1);
+			List<Menu> cmenus = menuService.findAllMenus(view2);
+			Object[] objs = {parent,cmenus};
+			ApplicationDataManager.SYSMENUS.add(objs);
+		}
+	}
+	
 	@Autowired
 	private IApiService apiService;
 	@Autowired
 	private ISysParamService sysParmService;
+	@Autowired
+	private IMenuService menuService;
 }

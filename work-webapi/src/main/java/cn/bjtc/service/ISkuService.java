03aea41e57983669f84df46818df9ecd0a371e43
潘松鹏 <bjpansongpeng@163.com>
2017-04-21 17:41:51 +0800

    sku

---
+++ work-webapi/src/main/java/cn/bjtc/service/ISkuService.java
@@ -0,0 +1,14 @@
+package cn.bjtc.service;
+
+import java.util.List;
+
+import cn.bjtc.model.Sku;
+import cn.bjtc.view.SkuView;
+
+public interface ISkuService {
+
+	public int saveSku(SkuView view);
+	public int updateSku(SkuView view);
+    public List<Sku> findAllSkus(SkuView view);
+	public Integer countAllSkus(SkuView view);
+}

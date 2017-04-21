2983e138b3eea97a96974241aecccf021423151d
xinxin <xinxin@LAPTOP-62RUSU57>
2017-04-21 16:41:30 +0800

    产品管理更新

--- work-webapi/src/main/java/cn/bjtc/controller/ProdController.java
+++ work-webapi/src/main/java/cn/bjtc/controller/ProdController.java
@@ -3,15 +3,15 @@ package cn.bjtc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.bjtc.api.ApiManager;
+import cn.bjtc.annotation.SysLogger;
 import cn.bjtc.api.ApiParam;
 import cn.bjtc.api.ApiReturn;
 import cn.bjtc.api.util.ParamUtil;
+import cn.bjtc.aspect.AspectType;
 import cn.bjtc.service.IProdService;
 import cn.bjtc.view.ProductView;
 
@@ -20,8 +20,9 @@ import cn.bjtc.view.ProductView;
 public class ProdController extends BaseController{
 
 	@RequestMapping(value="all", method=RequestMethod.POST)
+	@SysLogger(content="查询产品信息",type=AspectType.CONTROLLER)
 	public ApiReturn showProds(){
-		ApiParam param=ApiManager.getInstance().getParameters(request);
+		ApiParam param=findApiParam();
 		ProductView view=(ProductView) ParamUtil.convertToView(param, ProductView.class);
 		int count=prodService.countAllProds(view);
 		List<?> prods=prodService.findAllProds(view);
@@ -31,16 +32,18 @@ public class ProdController extends BaseController{
 	}
 	
 	@RequestMapping(value="create", method=RequestMethod.POST)
+	@SysLogger(content="新增产品",type=AspectType.CONTROLLER)
 	public ApiReturn execAddProd(){
-		ApiParam param=ApiManager.getInstance().getParameters(request);
+		ApiParam param=findApiParam();
 		ProductView view=(ProductView) ParamUtil.convertToView(param, ProductView.class);
 		prodService.saveProd(view);
 		return apiReturn;	
 	}
 	
 	@RequestMapping(value="update", method=RequestMethod.POST)
+	@SysLogger(content="更新产品信息",type=AspectType.CONTROLLER)
 	public ApiReturn execUpdateProd(){
-		ApiParam param=ApiManager.getInstance().getParameters(request);
+		ApiParam param=findApiParam();
 		ProductView view=(ProductView) ParamUtil.convertToView(param, ProductView.class);
 		prodService.updateProd(view);
 		return apiReturn;
@@ -49,10 +52,14 @@ public class ProdController extends BaseController{
 	
 	@RequestMapping(value="get", method=RequestMethod.POST)
 	public ApiReturn execeditProd(){
-		ApiParam  param=ApiManager.getInstance().getParameters(request);
+		try {
+		ApiParam  param=findApiParam();
 		ProductView view=(ProductView) ParamUtil.convertToView(param, ProductView.class);
 		List<?> prods=prodService.findAllProds(view);
 		apiReturn.setData(prods);
+		} catch (Exception e) {
+			showServerError();
+		}
 		return apiReturn;
 	}
 

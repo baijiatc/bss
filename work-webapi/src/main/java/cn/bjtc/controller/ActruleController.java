787dab5d622bc37464662ed40e3ac44cbbde1f2f
xinxin <xinxin@LAPTOP-62RUSU57>
2017-04-26 16:32:35 +0800

    日志功能的添加

--- work-webapi/src/main/java/cn/bjtc/controller/ActruleController.java
+++ work-webapi/src/main/java/cn/bjtc/controller/ActruleController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import cn.bjtc.annotation.SysLogger;
 import cn.bjtc.api.ApiParam;
 import cn.bjtc.api.ApiReturn;
 import cn.bjtc.api.util.ParamUtil;
+import cn.bjtc.aspect.AspectType;
 import cn.bjtc.service.IActruleService;
 import cn.bjtc.view.ActruleView;
 
@@ -18,6 +20,7 @@ import cn.bjtc.view.ActruleView;
 public class ActruleController extends BaseController{
 	
 	@RequestMapping(value="all", method=RequestMethod.POST)
+	@SysLogger(content="查询活动规则信息",type=AspectType.CONTROLLER)
 	public ApiReturn showActrules(){
 		try {
 			ApiParam param=findApiParam();
@@ -33,6 +36,7 @@ public class ActruleController extends BaseController{
 	}
 	
 	@RequestMapping(value="create", method=RequestMethod.POST)
+	@SysLogger(content="新增活动规则信息",type=AspectType.CONTROLLER)
 	public ApiReturn execAddActrule(){
 		try {
 			ApiParam param=findApiParam();
@@ -46,6 +50,7 @@ public class ActruleController extends BaseController{
 	}
 	
 	@RequestMapping(value="update", method=RequestMethod.POST)
+	@SysLogger(content="更新活动规则信息",type=AspectType.CONTROLLER)
     public ApiReturn execUpdateActrule(){
 		try {
 			ApiParam param=findApiParam();

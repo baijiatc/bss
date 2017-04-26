3d95a8e1beaaef175570e98100fe5ac60f15faba
xinxin <xinxin@LAPTOP-62RUSU57>
2017-04-26 16:02:36 +0800

    活动服务接口更新

--- work-webapi/src/main/java/cn/bjtc/service/IActyService.java
+++ work-webapi/src/main/java/cn/bjtc/service/IActyService.java
@@ -2,13 +2,13 @@ package cn.bjtc.service;
 
 import java.util.List;
 
-import cn.bjtc.model.Activity;
 import cn.bjtc.view.ActivityView;
 
 public interface IActyService {
 
+	
 	public int saveActy(ActivityView  view);
 	public int updateActy(ActivityView view);
-	public List<Activity> findAllActys(ActivityView view);
+	public List<ActivityView> findAllActys(ActivityView view);
 	public Integer countAllActys(ActivityView view);
 }

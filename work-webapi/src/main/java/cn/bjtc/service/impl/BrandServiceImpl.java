c467aee82151bf0ffe5c98dffabaab46b3069dcb
xinxin <xinxin@LAPTOP-62RUSU57>
2017-04-26 14:30:46 +0800

    字典管理和品牌管理的日志添加和状态翻译

--- work-webapi/src/main/java/cn/bjtc/service/impl/BrandServiceImpl.java
+++ work-webapi/src/main/java/cn/bjtc/service/impl/BrandServiceImpl.java
@@ -1,14 +1,20 @@
 package cn.bjtc.service.impl;
 
+
+
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.bjtc.dao.IBrandDAO;
 import cn.bjtc.model.Brand;
+import cn.bjtc.model.Role;
 import cn.bjtc.service.IBrandService;
 import cn.bjtc.view.BrandView;
+import cn.bjtc.view.RoleView;
 
 @Service("brandService")
 public class BrandServiceImpl  implements  IBrandService{
@@ -21,10 +27,19 @@ public class BrandServiceImpl  implements  IBrandService{
 		return brandDao.updateBrand(view);
 	}
 
-	public List<Brand> findAllBrands(BrandView view) {
-		return brandDao.findAllBrands(view);
+	public List<BrandView> findAllBrands(BrandView view) {
+		List<Brand> brands =brandDao.findAllBrands(view);
+		if(brands == null || brands.size() <= 0){
+			return new ArrayList<BrandView>(0);
+		}
+		List<BrandView> views = new ArrayList<BrandView>(brands.size());
+		for(Brand brand : brands){
+			BrandView brandView = new BrandView();
+			BeanUtils.copyProperties(brand, brandView);
+			views.add(brandView);
+		}
+		return views;
 	}
-
 	public Integer countAllBrands(BrandView view) {
 		return brandDao.countAllBrands(view);
 	}

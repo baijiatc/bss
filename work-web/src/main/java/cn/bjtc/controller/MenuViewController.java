519fa3832e05a1a22237542a2eed7cd3a5f44948
xinxin <xinxin@LAPTOP-62RUSU57>
2017-04-26 10:19:11 +0800

    功能权限更新

--- work-web/src/main/java/cn/bjtc/controller/MenuViewController.java
+++ work-web/src/main/java/cn/bjtc/controller/MenuViewController.java
@@ -6,21 +6,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cn.bjtc.annotation.SysPrivilege;
+
 @Controller 
 @RequestMapping("menu")
 public class MenuViewController {
 	
 	@RequestMapping(value="show.html",method=RequestMethod.GET)
+	@SysPrivilege(name="menu:show")
 	public String showMenuView(){
 		return "menu/list";
 	}
 	
 	@RequestMapping(value="add.html",method=RequestMethod.GET)
+	@SysPrivilege(name="menu:add")
 	public String showMenuAddView(){
 		return "menu/add";
 	}
 	
 	@RequestMapping(value="{menuid}.html",method=RequestMethod.GET)
+	@SysPrivilege(name="menu:add")
 	public String showMenuEditView(@PathVariable Integer menuid,Model model){
 		model.addAttribute("menuid", menuid);
 		return "menu/edit";
--- work-web/src/main/java/cn/bjtc/controller/SyslogViewController.java
+++ work-web/src/main/java/cn/bjtc/controller/SyslogViewController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cn.bjtc.annotation.SysPrivilege;
+
 @Controller
+@RequestMapping("syslog")
 public class SyslogViewController {
 
-	@RequestMapping(value="syslog/show.html",method=RequestMethod.GET)
+	@RequestMapping(value="show.html",method=RequestMethod.GET)
+	@SysPrivilege(name="log:show")
 	public String showSyslogView(){
 		return "syslog/list";
 	}

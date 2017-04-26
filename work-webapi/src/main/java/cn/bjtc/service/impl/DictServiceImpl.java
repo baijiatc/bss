d7ee27f045cfc98e5a6e7870449f77a95dfbc3b5
xinxin <xinxin@LAPTOP-62RUSU57>
2017-04-26 09:29:12 +0800

    字典服务借口实现类更新

--- work-webapi/src/main/java/cn/bjtc/service/impl/DictServiceImpl.java
+++ work-webapi/src/main/java/cn/bjtc/service/impl/DictServiceImpl.java
@@ -1,13 +1,17 @@
 package cn.bjtc.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.bjtc.dao.IDictionaryDao;
+import cn.bjtc.model.Activity;
 import cn.bjtc.model.Dictionary;
 import cn.bjtc.service.IDictService;
+import cn.bjtc.view.ActivityView;
 import cn.bjtc.view.DictionaryView;
 
 @Service("dictionaryService")
@@ -21,8 +25,18 @@ public class DictServiceImpl  implements  IDictService{
 		return dictionaryDao.updateDict(view);
 	}
 
-	public List<Dictionary> findAllDicts(DictionaryView view) {
-		return dictionaryDao.findAllDicts(view);
+	public List<DictionaryView> findAllDicts(DictionaryView view) {
+		List<Dictionary> dicts = dictionaryDao.findAllDicts(view);
+		if(dicts == null ||dicts.size() <= 0){
+			return new ArrayList<DictionaryView>(0);
+		}
+		List<DictionaryView> views = new ArrayList<DictionaryView>(dicts.size());
+		for(Dictionary dict : dicts){
+			DictionaryView dictView = new DictionaryView();
+			BeanUtils.copyProperties(dict, dictView);
+			views.add(dictView);
+		}
+		return views;
 	}
 
 	public Integer countAllDicts(DictionaryView view) {

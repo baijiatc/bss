42d806f5bc59e88a03368652c58ce867104af36f
xinxin <xinxin@LAPTOP-62RUSU57>
2017-04-26 15:50:40 +0800

    活动状态翻译以及日志的添加

--- work-webapi/src/main/java/cn/bjtc/service/impl/ActyServiceImpl.java
+++ work-webapi/src/main/java/cn/bjtc/service/impl/ActyServiceImpl.java
@@ -1,14 +1,18 @@
 package cn.bjtc.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.bjtc.dao.IActivityDao;
 import cn.bjtc.model.Activity;
+import cn.bjtc.model.Menu;
 import cn.bjtc.service.IActyService;
 import cn.bjtc.view.ActivityView;
+import cn.bjtc.view.MenuView;
 
 @Service("activityService")
 public class ActyServiceImpl implements  IActyService {
@@ -21,9 +25,20 @@ public class ActyServiceImpl implements  IActyService {
 		return  actyDao.updateActy(view);
 	}
 
-	public List<Activity> findAllActys(ActivityView view) {
-		return actyDao.findAllActys(view);
+	public List<ActivityView> findAllActys(ActivityView view) {
+		List<Activity> acts = actyDao.findAllActys(view);
+		if(acts == null ||acts.size() <= 0){
+			return new ArrayList<ActivityView>(0);
+		}
+		List<ActivityView> views = new ArrayList<ActivityView>(acts.size());
+		for(Activity act : acts){
+			ActivityView actView = new ActivityView();
+			BeanUtils.copyProperties(act, actView);
+			views.add(actView);
+		}
+		return views;
 	}
+
 
 	public Integer countAllActys(ActivityView view) {
 		return actyDao.countAllActys(view);

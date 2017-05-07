7ec116c8c0fef1dc84e5e8f8ff72ea445583fb9b
fischerdong <defix34@163.com>
2017-05-07 09:36:02 +0800

    库存提醒

--- work-schedule/src/main/java/cn/bjtc/dao/ITransDAO.java
+++ work-schedule/src/main/java/cn/bjtc/dao/ITransDAO.java
@@ -1,10 +1,14 @@
 package cn.bjtc.dao;
 
+import java.util.List;
+
 public interface ITransDAO {
 
 	public void transCustomers();
 	public void transShops();
 	public void transMoneyFlows();
 	public void transOrders();
+	
+	public List<?> monitorStock();
 	
 }
